9d52aaf2cdada4cef7d12a58afebf7cfff032e94
Raphael Almeida <jaguarnet7@gmail.com>
2020-10-02 15:54:55 -0300

    Initial tests settings

---
+++ tests/Filters/FactoryTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Filters;
+
+use PHPUnit\Framework\TestCase;
+use PHPCleanup\Sanitize;
+
+final class FactoryTest extends TestCase{
+
+    public function testCallUnexistFilterThrowException(){
+
+        $this->expectException(\Exception::class);
+
+        Sanitize::unExistFilter()->sanitize("rawData");
+    }
+}
